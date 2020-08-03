f98a768fa6369df3400fee4b8d74fac659f569e3
Diego Mazzieri <diego.maz97@gmail.com>
2020-08-03 19:47:20 +0200

    Restore metrics.py from overwritten __init__.py

--- src/text_processing/tokenizers.py
+++ src/text_processing/tokenizers.py
@@ -1,11 +1,14 @@
 import tensorflow_datasets as tfds
 from text_processing.divine_comedy import DIVINE_COMEDY, DIVIDING_SYMBOL, MARKERS
 
+
 def get_punctuation(text=DIVINE_COMEDY):
     return list(set([char for char in text if char != DIVIDING_SYMBOL and not char.isalnum()]))
 
+
 def get_words(text=DIVINE_COMEDY):
     return tfds.features.text.Tokenizer().tokenize(text)
+
 
 def char_tokenizer(text=DIVINE_COMEDY):
     vocabulary = list(set([c for c in ''.join(get_words(text))])) + get_punctuation(text) + list(MARKERS.values())
@@ -17,6 +20,7 @@ def char_tokenizer(text=DIVINE_COMEDY):
         decode_token_separator=''
     )
 
+
 def word_tokenizer(text=DIVINE_COMEDY):
     reserved_tokens = list(MARKERS.values()) + get_punctuation(text)
     tokenizer = tfds.features.text.Tokenizer(alphanum_only=False, reserved_tokens=reserved_tokens)
@@ -26,6 +30,7 @@ def word_tokenizer(text=DIVINE_COMEDY):
         strip_vocab=False,
         decode_token_separator=''
     )
+
 
 def subword_tokenizer(text=DIVINE_COMEDY, target_vocab_size=2048, max_subword_length=3):
     return tfds.features.text.SubwordTextEncoder.build_from_corpus(
