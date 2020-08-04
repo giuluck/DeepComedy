186f66e4e06166bbef83ec3994661bcb1ec9f449
Luca Giuliani <luca.giuliani10@studio.unibo.it>
2020-08-04 02:14:06 +0200

    Choose correct encoding for divine_comedy.txt

--- src/utils/resources.py
+++ src/utils/resources.py
@@ -2,4 +2,4 @@ DIVINE_COMEDY = 'divine comedy not loaded yet:\ncall utils.resources.load_divine
 
 def load_divine_comedy(path='../res/divine_comedy.txt'):
     global DIVINE_COMEDY
-    DIVINE_COMEDY = open(path, 'r').read()
+    DIVINE_COMEDY = open(path, 'r', encoding='ISO-8859-1').read()
